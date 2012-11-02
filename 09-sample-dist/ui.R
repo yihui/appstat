033e3204ea2e1d0286409f098455997f38ee3307
Yihui Xie <xie@yihui.name>
2012-11-02 09:56:27 -0500

    add the meaning of parameters in the UI

--- 09-sample-dist/ui.R
+++ 09-sample-dist/ui.R
@@ -11,7 +11,13 @@ shinyUI(pageWithSidebar(
     helpText('This module shows the sampling distribution of a variety of
              statistics such as the mean, variance, median and so on.'),
     selectInput('dist', 'Population distribution:',
-                choices = c('Binomial', 'Poisson', 'Normal', 'Exponential', 'Gamma')),
+                choices = c(
+                  'Binomial (n = param1, p = param2)',
+                  'Poisson (lambda = param1)',
+                  'Normal (mu = param1, sigma = param2)',
+                  'Exponential (lambda = param1)',
+                  'Gamma (shape = param1, scale = param2)'
+                )),
     sliderInput('param1', 'Parameter 1', min = 0, max = 10, value = 5, step = .1),
     sliderInput('param2', 'Parameter 2', min = 0, max = 10, value = .3, step = .1),
     sliderInput('n', 'Sample size', min = 2, max = 100, value = 30, step = 1),
