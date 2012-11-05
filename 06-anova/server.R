19c84dc445f4270c091008d2f774957c15629c54
Yihui Xie <xie@yihui.name>
2012-11-05 17:45:20 -0600

    switch the order of mu1, mu2 and m3, per suggestion of Tal and Heike

--- 06-anova/server.R
+++ 06-anova/server.R
@@ -11,7 +11,7 @@ shinyServer(function(input, output) {
   gen_data = reactive(function() {
     n = input$n; mu1 = input$mu1; mu2 = input$mu2; mu3 = input$mu3; s = input$sigma
     data.frame(y = c(rnorm1(n, mu1, s), rnorm2(n, mu2, s), rnorm3(n, mu3, s)),
-               group = rep(sprintf('mu%s', 1:3), each = n))
+               group = factor(rep(sprintf('mu%s', 1:3), each = n), levels = sprintf('mu%s', 3:1)))
   })
 
   output$aovPlot = reactivePlot(function() {
@@ -19,7 +19,7 @@ shinyServer(function(input, output) {
     par(mar = c(4, 4, .1, .1), mgp = c(2.8, 1, 0), las = 1)
     plot(y ~ group, data = d, border = 'gray', horizontal = TRUE)
     points(group ~ y, data = d, col = rgb(0, 0, 0, .8))
-    points(c(input$mu1, input$mu2, input$mu3), 1:3, pch = '|', cex = 2, col = 'red')
+    points(c(input$mu1, input$mu2, input$mu3), 3:1, pch = '|', cex = 2, col = 'red')
   }, width = 600, height = 300)
 
   output$aovSummary = reactivePrint(function() {
