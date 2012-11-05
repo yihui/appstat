5660d389b99d5bd7c6c118b65c6d7378c1b640c2
Yihui Xie <xie@yihui.name>
2012-11-05 17:58:29 -0600

    users can specify the value of x instead of fixing it at 0

--- 01-normal/server.R
+++ 01-normal/server.R
@@ -3,18 +3,18 @@ library(shiny)
 shinyServer(function(input, output) {
 
   output$normalPlot = reactivePlot(function() {
-    mu = input$mu; s = input$sigma
+    mu = input$mu; s = input$sigma; xval = input$xval
     par(mar = c(4, 4, .1, .1))
     x = seq(qnorm(.001, mu, s), qnorm(.999, mu, s), length = 100)
     plot(x, y <- dnorm(x, mu, s), type = 'n', ylab = 'density', ylim = c(0, .5))
-    if (max(x) > 0) {
-      x0 = seq(0, max(x), length = 30)
+    if (max(x) > xval) {
+      x0 = seq(xval, max(x), length = 30)
       y0 = c(dnorm(x0, mu, s), rep(0, length(x0)))
       x0 = c(x0, rev(x0))
       polygon(x0, y0, col = 'lightgray', border = NA)
-      p = pnorm(0, mu, s, lower.tail = FALSE)
+      p = pnorm(xval, mu, s, lower.tail = FALSE)
     } else p = 0
-    legend('topright', sprintf('P(X > 0) = %.04f', p), fill = 'lightgray', bty = 'n')
+    legend('topright', sprintf('P(X > %s) = %.04f', xval, p), fill = 'lightgray', bty = 'n')
     lines(x, y); box()
   }, width = 500, height = 400)
 
--- 01-normal/ui.R
+++ 01-normal/ui.R
@@ -9,10 +9,11 @@ shinyUI(pageWithSidebar(
       tags$script(src = 'https://c328740.ssl.cf1.rackcdn.com/mathjax/2.0-latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML', type = 'text/javascript')
     ),
     helpText('This module demonstrates the Normal distribution
-             \\(N(\\mu, \\sigma^2)\\) and the probability \\(P(X > 0)\\)
+             \\(N(\\mu, \\sigma^2)\\) and the probability \\(P(X > x)\\)
              (in the shaded area).'),
     sliderInput('mu', '\\(\\mu\\)', min = -3, max = 3, value = 0, step = .1),
     sliderInput('sigma', '\\(\\sigma\\)', min = .8, max = 2, value = 1, step = .05),
+    numericInput('xval', '\\(x\\)', value = 0, step = .05)
   ),
 
   mainPanel(
