e1860280fc52b58eeda3f3b33424b77c7e000555
Yihui Xie <xie@yihui.name>
2012-11-07 15:41:57 -0600

    move x below mu, and change it to a slider, per suggestion of Tal

--- 01-normal/ui.R
+++ 01-normal/ui.R
@@ -12,8 +12,8 @@ shinyUI(pageWithSidebar(
              \\(N(\\mu, \\sigma^2)\\) and the probability \\(P(X > x)\\)
              (in the shaded area).'),
     sliderInput('mu', '\\(\\mu\\)', min = -3, max = 3, value = 0, step = .1),
-    sliderInput('sigma', '\\(\\sigma\\)', min = .8, max = 2, value = 1, step = .05),
-    numericInput('xval', '\\(x\\)', value = 0, step = .05)
+    sliderInput('xval', '\\(x\\)', min = -5, max = 5, value = 0, step = .05),
+    sliderInput('sigma', '\\(\\sigma\\)', min = .8, max = 2, value = 1, step = .05)
   ),
 
   mainPanel(
