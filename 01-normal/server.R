249a8a475e3a6aef88abaaa18f42f1f200bf4138
Yihui Xie <xie@yihui.name>
2012-12-29 13:32:51 -0600

    P(x1 < X < x2) instead of P(X > x), and let x1 / x2 be infinity

--- 01-normal/server.R
+++ 01-normal/server.R
@@ -4,17 +4,19 @@ shinyServer(function(input, output) {
 
   output$normalPlot = reactivePlot(function() {
     mu = input$mu; s = input$sigma; xval = input$xval
-    par(mar = c(4, 4, .1, .1))
-    x = seq(qnorm(.001, mu, s), qnorm(.999, mu, s), length = 100)
+    if (input$x1inf) xval[1] = qnorm(.0000001, mu, s)
+    if (input$x2inf) xval[2] = qnorm(.9999999, mu, s)
+    par(mar = c(4, 0, .1, 0))
+    x = seq(-5, 5, length = 100)
     plot(x, y <- dnorm(x, mu, s), type = 'n', ylab = 'density', ylim = c(0, .5))
-    if (max(x) > xval) {
-      x0 = seq(xval, max(x), length = 30)
-      y0 = c(dnorm(x0, mu, s), rep(0, length(x0)))
-      x0 = c(x0, rev(x0))
-      polygon(x0, y0, col = 'lightgray', border = NA)
-      p = pnorm(xval, mu, s, lower.tail = FALSE)
-    } else p = 0
-    legend('topright', sprintf('P(X > %s) = %.04f', xval, p), fill = 'lightgray', bty = 'n')
+    x0 = seq(xval[1], xval[2], length = 30)
+    y0 = c(dnorm(x0, mu, s), rep(0, length(x0)))
+    x0 = c(x0, rev(x0))
+    polygon(x0, y0, col = 'lightgray', border = NA)
+    p = abs(diff(pnorm(xval, mu, s)))
+    text(mean(xval), max(y0)/2,
+         sprintf('P(%s < X < %s) = %.04f', if (input$x1inf) -Inf else xval[1],
+                 if (input$x2inf) Inf else xval[2], p))
     lines(x, y); box()
     abline(v = mu, col = 'red')
   }, width = 500, height = 400)
--- 01-normal/ui.R
+++ 01-normal/ui.R
@@ -9,14 +9,19 @@ shinyUI(pageWithSidebar(
       tags$script(src = 'https://c328740.ssl.cf1.rackcdn.com/mathjax/2.0-latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML', type = 'text/javascript')
     ),
     helpText('This module demonstrates the Normal distribution
-             \\(N(\\mu, \\sigma^2)\\) and the probability \\(P(X > x)\\)
+             \\(N(\\mu, \\sigma^2)\\) and the probability \\(P(x_1 < X < x_2)\\)
              (in the shaded area).'),
     sliderInput('mu', '\\(\\mu\\)', min = -3, max = 3, value = 0, step = .1),
-    sliderInput('xval', '\\(x\\)', min = -5, max = 5, value = 0, step = .05),
-    sliderInput('sigma', '\\(\\sigma\\)', min = .8, max = 2, value = 1, step = .05)
+    sliderInput('sigma', '\\(\\sigma\\)', min = .8, max = 2, value = 1, step = .1),
+    helpText('You can set \\(x_1\\) to \\(-\\infty\\), and/or \\(x_2\\) to \\(+\\infty\\)
+             to get one-sided probabilities.'),
+    checkboxInput('x1inf', '\\(x_1=-\\infty\\)'),
+    checkboxInput('x2inf', '\\(x_2=+\\infty\\)')
   ),
 
   mainPanel(
-    plotOutput('normalPlot', height = 'auto')
+    plotOutput('normalPlot', height = 'auto'),
+    div(sliderInput('xval', '\\(x_1,x_2\\)', min = -5, max = 5, value = c(0, 5), step = .1),
+        style = 'width: 500px')
   )
 ))
