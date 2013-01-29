94787259c33d6e2b94c63431399b87dd8055861f
Yihui Xie <xie@yihui.name>
2013-01-29 16:26:20 -0600

    use sliderInput instead of numericInput

--- 02-ols/ui.R
+++ 02-ols/ui.R
@@ -12,10 +12,10 @@ shinyUI(pageWithSidebar(
              $$Y_i=\\beta_0+\\beta_1x_i+\\epsilon_i$$ where \\(\\epsilon \\sim
              N(0, I_{n \\times n})\\). You can guess estimates of
              \\(\\beta_0\\) (intercept) and \\(\\beta_1\\) (slope) below.'),
-    numericInput('b0', '\\(\\beta_0\\)', min = -5, max = 5,
-                 value = round(runif(1, -2, 2), 1), step = .1),
-    numericInput('b1', '\\(\\beta_1\\)', min = -5, max = 5,
-                 value = round(runif(1, -2, 2), 1), step = .1),
+    sliderInput('b0', '\\(\\beta_0\\)', min = -5, max = 5,
+                value = round(runif(1, -2, 2), 1), step = .1),
+    sliderInput('b1', '\\(\\beta_1\\)', min = -5, max = 5,
+                value = round(runif(1, -2, 2), 1), step = .1),
     checkboxInput('showFit', 'Show OLS fit'),
     checkboxInput('showResid', 'Show residuals'),
     helpText('The red bar on the right indicates the Error Sum of Squares (SSE),
